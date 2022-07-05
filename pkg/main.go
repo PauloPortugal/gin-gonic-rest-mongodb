125089ac5e59b1bf720abbbcd18ee3bf78b61a04
Paulo Monteiro <paulo.from.portugal@gmail.com>
2022-07-05 16:08:59 +0100

    Add Swagger OpenAPI specs

--- pkg/main.go
+++ pkg/main.go
@@ -1,3 +1,29 @@
+//go:generate swagger generate spec
+
+// Documentation of our Books API
+//
+//	   Simple Gin API
+//
+//     Schemes: http
+//     Host: localhost:8080
+//     BasePath: /
+//     Version: 1.0.0
+//     Contact: Test User <some_email@example.com> http://github.com/
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//
+// swagger:meta
 package main
 
 import (
--- pkg/model/book.go
+++ pkg/model/book.go
@@ -2,18 +2,55 @@ package model
 
 import "time"
 
+// Book represents a book
+//
+// swagger:model
 type Book struct {
-	ID             string        `json:"id"`
-	Name           string        `json:"name"`
-	Author         string        `json:"author"`
-	Publisher      string        `json:"publisher"`
-	PublishedAt    PublishedDate `json:"published_at"`
-	Tags           []string      `json:"tags"`
-	Review         float32       `json:"review"`
-	SubmissionDate time.Time     `json:"submission_date"`
+	// the id for this book
+	// required: true
+	// min: 1
+	ID string `json:"id"`
+
+	// the name of the book
+	// required: true
+	// min length: 3
+	Name string `json:"name"`
+
+	// the book author's name
+	// required: true
+	// min length: 3
+	Author string `json:"author"`
+
+	// The publisher's name
+	// required: true
+	// min length: 3
+	Publisher string `json:"publisher"`
+
+	// The date the book was published
+	// required: true
+	PublishedAt PublishedDate `json:"published_at"`
+
+	// the associated tags with this book
+	Tags []string `json:"tags"`
+
+	// Score review
+	Review float32 `json:"review"`
+
+	//The day the book was added
+	SubmissionDate time.Time `json:"submission_date"`
 }
 
+// PublishedDate Represents the month and year of when the book was published
+//
+// swagger:model
 type PublishedDate struct {
+	// the month of the year
+	// required: true
+	// length: 2
 	Month string `json:"month"`
-	Year  string `json:"year"`
+
+	// the month of the year
+	// required: true
+	// min length: 1
+	Year string `json:"year"`
 }
