5255029ae5eaf336d8e0b742a002e1d33cbf3969
Paulo Monteiro <paulo.from.portugal@gmail.com>
2022-07-06 22:45:24 +0100

    Load static data and setup indexes

--- pkg/main.go
+++ pkg/main.go
@@ -28,12 +28,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/PauloPortugal/gin-gonic-rest-mongodb/pkg/handlers"
+	"github.com/PauloPortugal/gin-gonic-rest-mongodb/pkg/model"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -66,27 +70,91 @@ func readConfig() *viper.Viper {
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
-		panic(fmt.Errorf("fatal error config file: %w", err))
+		log.Fatal(fmt.Errorf("fatal error config file: %w", err))
 	}
 	return viper.GetViper()
 }
 
 func setupMongoDB(ctx context.Context, cfg *viper.Viper) *mongo.Client {
-	uri := fmt.Sprintf("mongodb://%s:%s@%s/%s?authSource=admin",
+	uri := fmt.Sprintf("mongodb://%s:%s@%s/test?authSource=admin",
 		cfg.Get("mongodb.dbuser"),
 		cfg.Get("mongodb.dbpassword"),
-		cfg.Get("mongodb.dbhost"),
-		cfg.Get("mongodb.dbname"))
+		cfg.Get("mongodb.dbhost"))
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
-		panic(fmt.Errorf("could not connect to databse: %w", err))
+		log.Fatal(fmt.Errorf("could not connect to databse: %w", err))
 	}
 
 	if err = client.Ping(ctx, readpref.Primary()); err != nil {
-		panic(fmt.Errorf("could not ping databse: %w", err))
+		log.Fatal(fmt.Errorf("could not ping databse: %w", err))
 	}
+
 	log.Println("Connected to MongoDB")
 
+	setupIndexes(ctx, client, cfg)
+
+	err = loadStaticData(ctx, client, cfg)
+	if err != nil {
+		log.Fatal(fmt.Errorf("could not insert static data: %w\n", err))
+		return nil
+	}
+
 	return client
+}
+
+func loadStaticData(ctx context.Context, client *mongo.Client, cfg *viper.Viper) error {
+	books := make([]model.Book, 0)
+
+	file, err := ioutil.ReadFile("books.json")
+	if err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(file, &books); err != nil {
+		return err
+	}
+
+	db := fmt.Sprint(cfg.Get("mongodb.dbname"))
+	col := fmt.Sprint(cfg.Get("mongodb.dbcollection"))
+
+	var b []interface{}
+	for _, book := range books {
+		b = append(b, book)
+	}
+	result, err := client.Database(db).Collection(col).InsertMany(ctx, b)
+	if err != nil {
+		if mongoErr, ok := err.(mongo.BulkWriteException); ok {
+			if len(mongoErr.WriteErrors) > 0 && mongoErr.WriteErrors[0].Code == 11000 {
+				return nil
+			}
+		}
+		return err
+	}
+
+	log.Printf("Inserted books: %d\n", len(result.InsertedIDs))
+
+	return nil
+}
+
+func setupIndexes(ctx context.Context, client *mongo.Client, cfg *viper.Viper) {
+	db := fmt.Sprint(cfg.Get("mongodb.dbname"))
+	col := fmt.Sprint(cfg.Get("mongodb.dbcollection"))
+
+	idxOpt := &options.IndexOptions{}
+	idxOpt.SetUnique(true)
+	mod := mongo.IndexModel{
+		Keys: bson.M{
+			"id": 1, // index in ascending order
+		},
+		Options: idxOpt,
+	}
+
+	ind, err := client.Database(db).Collection(col).Indexes().CreateOne(ctx, mod)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Indexes().CreateOne() ERROR: %w", err))
+	} else {
+		// API call returns string of the index name
+		log.Printf("CreateOne() index: %s\n", ind)
+	}
 }
