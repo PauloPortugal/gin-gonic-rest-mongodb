30308408d0402f7798c0e0e3bfaac3c82811c165
Paulo Monteiro <paulo.from.portugal@gmail.com>
2022-07-06 14:12:42 +0100

    Connect to local MongoDB

--- pkg/main.go
+++ pkg/main.go
@@ -27,11 +27,20 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/PauloPortugal/gin-gonic-rest-mongodb/pkg/handlers"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
+	ctx := context.Background()
+	setupMongoDB(ctx)
+
 	router := gin.Default()
 
 	router.POST("/books", handlers.NewBook)
@@ -44,4 +53,16 @@ func main() {
 	if err != nil {
 		return
 	}
+}
+
+func setupMongoDB(ctx context.Context) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://user_name:user_pass@localhost:27017/test?authSource=admin"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
 }
